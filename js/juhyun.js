054b6aeaa9bb01f975bebda04d53b13a5194a51a
juhyun88 <tls839@naver.com>
2025-04-09 11:47:21 +0900

    섹션 정리 및 자바스크립트 추가

---
+++ js/juhyun.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let lastScrollY = window.scrollY; 
+    const header = document.querySelector("#headerwarp");
+
+    if (!header) return; 
+
+    window.addEventListener('scroll', () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY) {
+            header.style.transform = "translateY(-100%)";
+        } else {
+            header.style.transform = "translateY(0)";
+        }
+
+        header.style.transition = "transform 0.3s ease-in-out"; 
+        lastScrollY = currentScrollY; 
+        });
+    });
+  $(function() {
+    if ($(".doctor-list").length) { // .main-info-list가 존재할 때만 실행
+        $(".doctor-list").slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: false,
+            dots: false,
+            autoplay: true,
+            speed: 1000,
+            infinite: true,
+            autoplaySpeed: 3000,
+            easing: 'easeInOutQuint',
+            pauseOnHover: false,
+            centerMode: true,
+            zIndex: 1,
+            responsive: [
+                {
+                    breakpoint: 1221,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+            ]
+        });
+    }
+});
+                                
