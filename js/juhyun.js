3b499928576e11905a6d6119cd936da7539f7a8a
juhyun88 <tls839@naver.com>
2025-04-09 16:27:58 +0900

    [의사들]이미지 사이즈 수정5

--- js/juhyun.js
+++ js/juhyun.js
@@ -17,32 +17,32 @@ document.addEventListener('DOMContentLoaded', () => {
         lastScrollY = currentScrollY; 
         });
     });
-  $(function() {
+$(function() {
     if ($(".doctor-list").length) { // .main-info-list가 존재할 때만 실행
-        $(".doctor-list").slick({
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: false,
-            dots: false,
-            autoplay: true,
-            speed: 1000,
-            infinite: true,
-            autoplaySpeed: 10000,
-            easing: 'easeInOutQuint',
-            pauseOnHover: false,
-            centerMode: true,
-            zIndex: 1,
-            responsive: [
-                {
-                    breakpoint: 1221,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                    }
-                },
-            ]
-        });
+            $(".doctor-list").slick({
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    arrows: true, // 화살표 버튼 활성화
+                    fade: false,
+                    dots: false,
+                    autoplay: true,
+                    speed: 1000,
+                    infinite: true,
+                    autoplaySpeed: 10000,
+                    easing: 'easeInOutQuint',
+                    pauseOnHover: false,
+                    centerMode: true,
+                    zIndex: 1,
+                    responsive: [
+                            {
+                                    breakpoint: 1221,
+                                    settings: {
+                                            slidesToShow: 3,
+                                            slidesToScroll: 1,
+                                    }
+                            },
+                    ]
+            });
     }
 });
                                 
