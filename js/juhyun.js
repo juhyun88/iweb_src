9c3432bfe10f162f55c8b51d028243d3ba7cd73c
juhyun88 <tls839@naver.com>
2025-04-10 17:55:32 +0900

    [첫번재배너]버튼 수정2

--- js/juhyun.js
+++ js/juhyun.js
@@ -48,19 +48,19 @@ $(function() {
 $(function () {
     if ($(".firstbanner-rightsection .rightsection_img").length) {
         $(".firstbanner-rightsection .rightsection_img").slick({
-            dots: true, // 하단 점 네비게이션 활성화
+         
             infinite: true, // 무한 루프 활성화
             arrows: true, // 화살표 버튼 활성화
             speed: 300, // 슬라이드 전환 속도
-            slidesToShow: 1, // 한 번에 보여줄 슬라이드 개수
+            slidesToShow: 2, // 한 번에 보여줄 슬라이드 개수
             slidesToScroll: 1, // 한 번에 움직일 슬라이드 개수
-            prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">←</button>', // 이전 화살표
-            nextArrow: '<button class="slick-next" aria-label="Next" type="button">→</button>', // 다음 화살표
+            prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"></button>', // 이전 화살표
+            nextArrow: '<button class="slick-next" aria-label="Next" type="button"></button>', // 다음 화살표
             responsive: [
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 1,
+                        slidesToShow: 2,
                         slidesToScroll: 1,
                         infinite: true,
                         dots: true,
@@ -69,14 +69,14 @@ $(function () {
                 {
                     breakpoint: 600,
                     settings: {
-                        slidesToShow: 1,
+                        slidesToShow: 2,
                         slidesToScroll: 1,
                     },
                 },
                 {
                     breakpoint: 480,
                     settings: {
-                        slidesToShow: 1,
+                        slidesToShow: 2,
                         slidesToScroll: 1,
                     },
                 },
