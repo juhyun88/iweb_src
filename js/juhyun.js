4b60b1b28fc006f8135382cec0a2f2001e9c14db
juhyun88 <tls839@naver.com>
2025-04-11 12:26:43 +0900

    [유튜브배너]글자 및 이미지 추가

--- js/juhyun.js
+++ js/juhyun.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
         lastScrollY = currentScrollY; 
         });
     });
+
+    //의사들 배너 슬라이드//
 $(function() {
     if ($(".doctor-list").length) { // .main-info-list가 존재할 때만 실행
             $(".doctor-list").slick({
@@ -45,6 +47,9 @@ $(function() {
             });
     }
 });
+
+
+//첫번째 배너 슬라이드//
 $(function () {
     if ($(".firstbanner-rightsection .rightsection_img").length) {
         $(".firstbanner-rightsection .rightsection_img").slick({
@@ -54,6 +59,49 @@ $(function () {
             arrows: true, // 화살표 버튼 활성화
             speed: 300, // 슬라이드 전환 속도
             slidesToShow: 2, // 한 번에 보여줄 슬라이드 개수
+            slidesToScroll: 1, // 한 번에 움직일 슬라이드 개수
+            prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"></button>', // 이전 화살표
+            nextArrow: '<button class="slick-next" aria-label="Next" type="button"></button>', // 다음 화살표
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true,
+                    },
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+        });
+    }
+});
+
+
+//유튜브 슬라이드//
+$(function () {
+    if ($("youtobe_section_img").length) {
+        $("youtobe_section_img").slick({
+            centerMode: false,
+            centerPadding: '0px',
+            infinite: true, // 무한 루프 활성화
+            arrows: true, // 화살표 버튼 활성화
+            speed: 300, // 슬라이드 전환 속도
+            slidesToShow: 4, // 한 번에 보여줄 슬라이드 개수
             slidesToScroll: 1, // 한 번에 움직일 슬라이드 개수
             prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"></button>', // 이전 화살표
             nextArrow: '<button class="slick-next" aria-label="Next" type="button"></button>', // 다음 화살표
